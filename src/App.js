6f7bbf8c26ae3a75b06ca7e8557e56bfb5a13b12
YJ <imyj7882@gmail.com>
2025-12-05 14:44:23 +0900

    yj

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { Outlet, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
+import DetailPage from './pages/DetailPage';
+import './App.css';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+const Layout = () =>{
+  return(
+    <div>
+      <Nav />
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<MainPage/>}/>
+          <Route path=':movieId' element={<DetailPage/>}/>
+          <Route path='search' element={<SearchPage/>}/>
+        </Route>
+
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useOnclickOutside.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+
+const useOnclickOutside = (ref, handler) => {
+    useEffect(() => {
+        const listener = (event) => {
+            if(!ref.current || ref.current.contains(event.target)){
+                return;
+            }
+            handler();
+        };     
+
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+        return () =>{
+        document.addEventListener("touchstart", listener);
+        document.addEventListener("mousedown", listener);
+        };
+    },[])
+};
+
+export default useOnclickOutside
